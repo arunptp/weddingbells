5e7ef56b7ce79459a01a21617e971ff65e62a92d
Arun Thomas <arunptp@gmail.com>
2019-10-20 18:55:09 +0530

    removing owl dots

--- js/main.js
+++ js/main.js
@@ -7,10 +7,7 @@ jQuery(document).ready(function( $ ) {
 
   jQuery(".player").mb_YTPlayer();
 
-  sizeTheVideo();
-  $(window).resize(function(){
-    sizeTheVideo();
-  });
+
 
 
 	"use strict";
@@ -354,18 +351,3 @@ jQuery(document).ready(function( $ ) {
 setInterval(function() { makeTimer(); }, 1000);
 
 });
-
-function sizeTheVideo(){
-  // - 1.78 is the aspect ratio of the video
-// - This will work if your video is 1920 x 1080
-// - To find this value divide the video's native width by the height eg 1920/1080 = 1.78
-  var aspectRatio = 1.78;
-
-    var video = $('#videoWithJs iframe');
-    var videoHeight = video.outerHeight();
-    var newWidth = videoHeight*aspectRatio;
-		var halfNewWidth = newWidth/2;
-
-  //Define the new width and centrally align the iframe
-  video.css({"width":newWidth+"px","left":"50%","margin-left":"-"+halfNewWidth+"px"});
-}
